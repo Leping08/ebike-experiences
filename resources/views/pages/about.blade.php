9becf5e4db7d7164361bf8c2edc0d715335a859e
Big-nuche <abenuche@yahoo.com>
2023-04-02 22:22:05 -0400

    theme changes on about and contact pages

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,18 +9,18 @@
 @section('content')
     {{-- @todo make this work in dark mode --}}
     <div class="container mx-auto my-10">
-        <h1 class="text-3xl font-bold mb-5">About Us</h1>
-        <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis quam vel elementum faucibus.
+        <h1 class="text-3xl dark:text-gray-200 font-bold mb-5">About Us</h1>
+        <p class="mb-5 dark:text-gray-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis quam vel elementum faucibus.
             Sed porttitor interdum felis, in dictum justo luctus sed. Donec feugiat sapien ut lorem malesuada, id eleifend
             libero maximus. Suspendisse ut purus blandit, pharetra lorem eu, luctus ipsum. Morbi sit amet malesuada est,
             quis auctor elit. Sed rhoncus quam ut semper molestie. Praesent ac sem bibendum, pharetra sapien a, blandit
             urna. Maecenas aliquam urna quis nibh venenatis, ut maximus neque aliquet. Etiam elementum ut turpis sit amet
             euismod. Quisque vel lectus ipsum. Sed sagittis malesuada diam, vitae varius nisi.</p>
-        <p class="mb-5">Nulla facilisi. In vitae sagittis risus. Nam vel lorem eu enim cursus eleifend at quis ipsum.
+        <p class="mb-5 dark:text-gray-200">Nulla facilisi. In vitae sagittis risus. Nam vel lorem eu enim cursus eleifend at quis ipsum.
             Suspendisse vitae rhoncus nisi. In eleifend dapibus risus a convallis. Duis quis euismod odio. Nulla facilisi.
             Maecenas gravida odio in risus hendrerit volutpat. Nam euismod purus vel neque consectetur, nec dapibus velit
             tristique.</p>
-        <p>Donec gravida sapien libero, vel blandit turpis imperdiet a. Curabitur sit amet metus magna. Donec ullamcorper
+        <p class="dark:text-gray-200">Donec gravida sapien libero, vel blandit turpis imperdiet a. Curabitur sit amet metus magna. Donec ullamcorper
             dui in arcu gravida viverra. Sed venenatis metus non enim hendrerit, quis rhoncus arcu malesuada. Fusce dictum
             lorem at tristique ultricies. Sed sed nunc id dolor euismod imperdiet. Sed euismod, lorem quis dignissim
             suscipit, augue nisl faucibus lacus, sit amet pharetra dolor mi nec sapien. Vivamus pulvinar eu enim vel varius.
