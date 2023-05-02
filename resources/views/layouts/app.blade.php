2c9b4126efc4b83c154a2bf54b13dd59be007d26
Big-nuche <abenuche@yahoo.com>
2023-05-01 21:22:03 -0400

    Updated comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('title') | {{ config('app.seo_name') }}</title>
     <meta name="description" content="@yield('description')">
     @vite('resources/css/app.css')
-    {{-- Favorite Icons --}}
+    <!-- Favorite Icons -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
