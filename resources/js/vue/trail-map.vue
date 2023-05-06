50c0d29f41d5976841e51edf880f8a26173a1a8d
Leping08 <dereks008@gmail.com>
2023-05-06 11:47:05 -0400

    Fix video id in map popup

--- resources/js/vue/trail-map.vue
+++ resources/js/vue/trail-map.vue
@@ -49,7 +49,7 @@
                             </div>
                         </div>
                         <div>
-                            <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${trailHead.videoId}`"
+                            <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${trailHead.video_id}`"
                                 frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen></iframe>
